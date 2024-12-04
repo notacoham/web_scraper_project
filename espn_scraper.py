bb371ab180ed44a7a51088f695bd0eb82f636b24
Alex Cottam <alexcottam12@gmail.com>
2024-12-04 16:50:10 -0700

    add print statements for all headers and content

--- espn_scraper.py
+++ espn_scraper.py
@@ -14,4 +14,15 @@ top_story = soup.find('div', class_='top_story')
 top_story_header = top_story.find('h1').text.replace('  ', '')
 top_story_subheader = top_story.find('h2').text.replace('  ', '')
 
-# 
+# find all stories on main page
+stories = soup.find_all('div', class_='headline')
+
+# print top header
+print(f'Top Story: {top_story_header}\n--------\n-->{top_story_subheader}\n')
+
+# print all stories
+for story in stories:
+    story_name = story.a.text
+    story_date = story.find('span', class_='short').text.replace('|', '')
+    story_content = story.h5.text
+    print(f'{story_name}\n->{story_date}\n-->{story_content}\n')
