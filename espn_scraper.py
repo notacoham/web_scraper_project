a5ab471545e947f034002049105029c2a7e8cac7
Alex Cottam <alexcottam12@gmail.com>
2024-12-05 12:55:34 -0700

    add final arrays to be written and append info into them

--- espn_scraper.py
+++ espn_scraper.py
@@ -17,12 +17,18 @@ top_story_subheader = top_story.find('h2').text.replace('  ', '')
 # find all stories on main page
 stories = soup.find_all('div', class_='headline')
 
-# print top header
-print(f'Top Story: {top_story_header}\n--------\n-->{top_story_subheader}\n')
+# initialize the final arrays
+stories_names = []
+stories_dates = []
+stories_content = []
 
 # print all stories
 for story in stories:
     story_name = story.a.text
-    story_date = story.find('span', class_='short').text.replace('|', '')
+    story_date = story.find('span', class_='short').text.replace('\n', '').replace('  ', '').replace('\xa0', '').replace('|', '')
     story_content = story.h5.text
-    print(f'-{story_name}\n->{story_date}\n{story_content}\n')
+    stories_names.append(story_name)
+    stories_dates.append(story_date)
+    stories_content.append(story_content)
+    
+print(stories_content)
